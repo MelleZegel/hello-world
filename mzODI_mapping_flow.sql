708bef12108090f0b960012861c839027840d8d6
MelleZegel <c@mellezegel.com>
2016-04-06 09:04:19 +0200

    Update mzODI_mapping_flow.sql

--- mzODI_mapping_flow.sql
+++ mzODI_mapping_flow.sql
@@ -1,34 +1,35 @@
 with mcn as (
 select m.name mapping
-     , comp.name comp_name, comp.type_name comp_type
+     , comp.NAME comp_name
+     , CASE comp.type_name 
+         WHEN 'AGGREGATE' THEN 'A'
+         WHEN 'DISTINCT' THEN 'D'
+         WHEN 'FILTER' THEN 'F'
+         WHEN 'JOIN' THEN 'J'
+         WHEN 'LOOKUP' THEN 'L'
+         WHEN 'SPLITTER' THEN 'P'
+         WHEN 'REUSABLEMAPPING' THEN 'R'
+         WHEN 'SET' THEN 'S'
+         WHEN 'DATASTORE' THEN 'T'
+         WHEN 'EXPRESSION' THEN 'X'
+         ELSE comp.type_name END comp_type
      , t.table_name
      , cps.i_owner_map_comp i_cps, cpe.i_owner_map_comp i_cpe
 --     , comp.i_map_ref, comp.i_owner_mapping
-FROM      snp_map_cp   cps   
-JOIN      snp_map_conn conn  ON cps.i_map_cp            = conn.i_start_map_cp 
-JOIN      snp_map_cp   cpe   ON cpe.i_map_cp            = conn.i_end_map_cp
-JOIN      snp_map_comp comp  ON cps.i_owner_map_comp    = comp.i_map_comp
-JOIN      snp_mapping  m     on m.i_mapping             = comp.i_owner_mapping
+FROM      snp_mapping  m     
+JOIN      snp_map_comp comp  ON m.i_mapping             = comp.i_owner_mapping
+JOIN      snp_map_cp   cps   ON cps.i_owner_map_comp    = comp.i_map_comp and cps.direction = 'O'
+LEFT JOIN snp_map_conn conn  ON cps.i_map_cp            = conn.i_start_map_cp 
+LEFT JOIN snp_map_cp   cpe   ON cpe.i_map_cp            = conn.i_end_map_cp
 LEFT JOIN snp_map_ref  mr    ON comp.i_map_ref          = mr.i_map_ref
 LEFT JOIN snp_table    t     ON mr.i_ref_id             = t.i_table
   )
-select level
-     ,  RPAD('.', (level-1)*2, '.') || comp_name AS tree
-     , LTRIM(SYS_CONNECT_BY_PATH(comp_name, '-'), '-') AS path
+SELECT LEVEL
+     , RPAD('.', (level-1)*2, '.') || comp_name AS tree
      , mcn.* 
-from   mcn
-connect by i_cps = prior i_cpe
-order by mapping, 1, comp_name
---------------------------------------------------------------------------------
-
---SELECT id,
---       parent_id,
---       RPAD('.', (level-1)*2, '.') || id AS tree,
---       level,
---       CONNECT_BY_ROOT id AS root_id,
---       LTRIM(SYS_CONNECT_BY_PATH(id, '-'), '-') AS path,
---       CONNECT_BY_ISLEAF AS leaf
---FROM   tab1
---START WITH parent_id IS NULL
---CONNECT BY parent_id = PRIOR id
---ORDER SIBLINGS BY id;
+     , ltrim(sys_connect_by_path(comp_name, '<-'), '-') AS path
+FROM   mcn
+WHERE  mapping LIKE 'MAP_BLABLA%'
+START WITH i_cpe IS NULL
+CONNECT BY i_cpe = PRIOR i_cps
+ORDER BY mapping, path
